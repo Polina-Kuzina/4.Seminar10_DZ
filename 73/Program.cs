75167ade2e1f51d3361bf870bff87b63a30d3b9b
Polina <payline@mail.ru>
2022-04-04 10:23:02 +0300

    Домашнее задание 10

---
+++ 73/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 73 : Есть число N. Сколько групп M, можно получить при разбиении всех чисел на группы, 
+// так чтобы в одной группе все числа были взаимно просты (все числа в группе друг на друга не делятся)? 
+// Найдите M при заданном N и получите одно из разбиений на группы N ≤ 10²⁰.
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+int m = Convert.ToInt32(Math.Log(n, 2));
+Console.WriteLine("Число групп = " + m);
+Console.WriteLine("Группа 1: 1");
+for (int i = 1; i < m; i++)
+{
+    Console.Write($"Группа {i + 1}: ");
+    for (int k = Convert.ToInt32(Math.Pow(2, i)); k < Convert.ToInt32(Math.Pow(2, (i + 1))) && k<(n+1); k++)
+    {
+        Console.Write(k + " ");
+    }
+    Console.WriteLine();
+}
